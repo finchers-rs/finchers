e32001113415354f7f26c794985465b09c08f9bb
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-08-18 03:49:48 +0900

    path param

--- src/endpoint/path.rs
+++ src/endpoint/path.rs
@@ -1,15 +1,18 @@
 use std::borrow::Cow;
+use std::marker::PhantomData;
+use std::str::FromStr;
+use futures::{Future, Poll, Async};
 use input::Input;
 use super::Endpoint;
 
 
 #[derive(Debug)]
-pub struct Path<E: Endpoint> {
+pub struct MatchPath<E: Endpoint> {
     name: Cow<'static, str>,
     endpoint: E,
 }
 
-impl<E: Endpoint> Endpoint for Path<E> {
+impl<E: Endpoint> Endpoint for MatchPath<E> {
     type Item = E::Item;
     type Error = E::Error;
     type Future = E::Future;
@@ -29,13 +32,6 @@ impl<E: Endpoint> Endpoint for Path<E> {
     }
 }
 
-pub fn path<S: Into<Cow<'static, str>>, E: Endpoint>(name: S, endpoint: E) -> Path<E> {
-    Path {
-        name: name.into(),
-        endpoint,
-    }
-}
-
 
 #[derive(Debug)]
 pub struct PathEnd<E: Endpoint> {
@@ -52,6 +48,60 @@ impl<E: Endpoint> Endpoint for PathEnd<E> {
             return Err(input);
         }
         self.endpoint.apply(input)
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Path<T: FromStr, E: Endpoint> {
+    endpoint: E,
+    _marker: PhantomData<T>,
+}
+
+impl<T: FromStr, E: Endpoint> Endpoint for Path<T, E> {
+    type Item = (T, E::Item);
+    type Error = E::Error;
+    type Future = PathFuture<T, E::Future>;
+
+    fn apply(&self, mut input: Input) -> Result<Self::Future, Input> {
+        let value: T = match input.routes.get(0).and_then(|s| s.parse().ok()) {
+            Some(v) => v,
+            None => return Err(input),
+        };
+        input.routes = input.routes.into_iter().skip(1).collect();
+        Ok(PathFuture {
+            value: Some(value),
+            future: self.endpoint.apply(input)?,
+        })
+    }
+}
+
+pub struct PathFuture<T, F: Future> {
+    value: Option<T>,
+    future: F,
+}
+
+impl<T, F: Future> Future for PathFuture<T, F> {
+    type Item = (T, F::Item);
+    type Error = F::Error;
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let rhs = try_ready!(self.future.poll());
+        let lhs = self.value.take().expect("cannot resolve twice");
+        Ok(Async::Ready((lhs, rhs)))
+    }
+}
+
+pub fn value<T: FromStr, E: Endpoint>(endpoint: E) -> Path<T, E> {
+    Path {
+        endpoint,
+        _marker: PhantomData,
+    }
+}
+
+pub fn path<S: Into<Cow<'static, str>>, E: Endpoint>(name: S, endpoint: E) -> MatchPath<E> {
+    MatchPath {
+        name: name.into(),
+        endpoint,
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(conservative_impl_trait)]
+
+#[macro_use]
 extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
@@ -13,22 +16,37 @@ use tokio_core::reactor::Core;
 
 use endpoint::Endpoint;
 use endpoint::param::param;
-use endpoint::path::{path, path_end};
+use endpoint::path::{path, path_end, value};
 
 use input::Input;
 use responder::Responder;
 
+/// "/foo/:id/bar?hello=<hello>"
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn endpoint() -> impl Endpoint<Item = (u64, String)> + std::fmt::Debug {
+    path("foo",
+        value::<u64, _>(
+            path("bar",
+                 path_end(param("hello")))))
+}
+
 fn main() {
-    let endpoint = path("foo", path("bar", path_end(param("hello"))));
+    let endpoint = endpoint();
     println!("endpoint: {:#?}", endpoint);
 
-    let input = Input::new(Get, "/foo/bar?hello=world");
+    let input = Input::new(Get, "/foo/42/bar?hello=world");
     println!("input: {:#?}", input);
     println!();
 
     if let Ok(f) = endpoint.apply(input) {
+        let f = f.map(|(id, hello)| format!("({}, {})", id, hello)).map(
+            |r| {
+                r.respond()
+            },
+        );
+
         let mut core = Core::new().unwrap();
-        let output = core.run(f.map(|r| r.respond()));
+        let output = core.run(f);
 
         match output {
             Ok(response) => {
@@ -45,10 +63,9 @@ fn main() {
                 );
                 println!("..with body: {:?}", body);
             }
-            Err(err) => eprintln!("failed with: {:?}", err),
+            Err(_err) => {}//eprintln!("failed with: {:?}", err),
         }
     } else {
         eprintln!("no route");
     }
 }
-
