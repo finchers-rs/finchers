36ce171543e121ec1ce84bebea2d4f3c5b4e5754
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-08-18 19:06:23 +0900

    update example

--- examples/hello.rs
+++ examples/hello.rs
@@ -9,29 +9,37 @@ use finch::combinator::{path, path_seq};
 use finch::endpoint::{Context, Endpoint};
 use finch::request::Request;
 
-use hyper::Get;
+use hyper::{Method, Get};
 use tokio_core::reactor::Core;
 
-fn main() {
+fn run_test<E: Endpoint>(
+    endpoint: &E,
+    method: Method,
+    uri: &str,
+) -> Result<Result<E::Item, E::Error>, ()> {
     let req = Request {
-        method: Get,
-        uri: "/foo/bar/42/baz/foo/bar/".parse().unwrap(),
+        method,
+        uri: uri.parse().unwrap(),
         headers: Default::default(),
     };
     let mut ctx = Context::new(&req, Default::default());
 
+    let f = endpoint.apply(&req, &mut ctx)?;
+
+    let mut core = Core::new().unwrap();
+    Ok(core.run(f))
+}
+
+
+fn main() {
     let endpoint = "foo".with("bar").with(path::<u32>()).join("baz".with(
         path_seq::<String>(),
     ));
-    let f = match endpoint.apply(&req, &mut ctx) {
-        Ok(f) => f,
-        Err(_) => {
-            eprintln!("no route");
-            return;
-        }
-    };
 
-    let mut core = Core::new().unwrap();
-    let result = core.run(f);
-    println!("{:?}", result);
+    println!("{:?}", run_test(&endpoint, Get, "/foo/bar/42/baz/foo/bar/"));
+    // => Ok(Ok((42, ["foo", "bar"])))
+
+
+    println!("{:?}", run_test(&endpoint, Get, "/foo/baz/42/baz/foo/bar/"));
+    // => Err(())
 }
