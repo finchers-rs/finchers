46e270a9a224811d9435b52bfaa0310959240cec
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-08-18 22:40:17 +0900

    update example

--- examples/hello.rs
+++ examples/hello.rs
@@ -43,17 +43,29 @@ enum Params {
     B,
 }
 
+impl From<(u32, Vec<String>)> for Params {
+    fn from(val: (u32, Vec<String>)) -> Self {
+        Params::A(val.0, val.1)
+    }
+}
+
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
 fn endpoint() -> impl Endpoint<Item = Params, Error = &'static str> + 'static {
+    // "/foo/bar/<id:u32>/baz/<seq...:[String]>" => Params::A(id, seq)
     let e1 = "foo"
         .with("bar")
         .with(path::<u32>())
         .skip("baz")
         .join(path_seq::<String>())
-        .map(|(id, seq)| Params::A(id, seq))
+        .map(Into::into)
         .map_err(|_| "endpoint 1");
-    let e2 = "hello".with("world").map(|_| Params::B).map_err(
-        |_| "endpoint 2",
-    );
+
+    // "/hello/world" => Params::B
+    let e2 = "hello"
+        .with("world")
+        .map(|_| Params::B)
+        .map_err(|_| "endpoint 2");
 
     get(e1).or(get(e2))
 }
@@ -63,20 +75,20 @@ fn main() {
         "{:?}",
         run_test(endpoint(), Get, "/foo/bar/42/baz/foo/bar/")
     );
-    // => Ok(Ok(Either::A(Params(42, ["foo", "bar"]))))
-
-    println!(
-        "{:?}",
-        run_test(endpoint(), Post, "/foo/bar/42/baz/foo/bar/")
-    );
-    // => Err(InvalidMethod)
+    // => Ok(Ok(Params::A(42, ["foo", "bar"])))
 
     println!("{:?}", run_test(endpoint(), Get, "/hello/world"));
-    // => Ok(Ok(Either::B(())))
+    // => Ok(Ok(Params::B))
 
     println!(
         "{:?}",
         run_test(endpoint(), Get, "/foo/baz/42/baz/foo/bar/")
     );
     // => Err(NoRoute)
+
+    println!(
+        "{:?}",
+        run_test(endpoint(), Post, "/foo/bar/42/baz/foo/bar/")
+    );
+    // => Err(InvalidMethod)
 }
