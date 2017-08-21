772ad0d70da39c6142732e19044a8b40bd4088ad
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-08-21 16:31:33 +0900

    docs: add documentation for TestCase

--- src/test.rs
+++ src/test.rs
@@ -10,14 +10,14 @@ use endpoint::Endpoint;
 use errors::*;
 use request::{Request, Body};
 
-#[allow(missing_docs)]
+/// A test case for `run_test()`
 pub struct TestCase {
     request: Request,
     body: Option<Body>,
 }
 
-#[allow(missing_docs)]
 impl TestCase {
+    /// Construct a `TestCase` from given HTTP method and URI
     pub fn new(method: Method, uri: &str) -> Self {
         let request = Request::new(method, uri).expect("invalid URI");
         Self {
@@ -26,31 +26,38 @@ impl TestCase {
         }
     }
 
+    /// Equivalent to `TestCase::new(Method::Get, uri)`
     pub fn get(uri: &str) -> Self {
         Self::new(Method::Get, uri)
     }
 
+    /// Equivalent to `TestCase::new(Method::Post, uri)`
     pub fn post(uri: &str) -> Self {
         Self::new(Method::Post, uri)
     }
 
+    /// Equivalent to `TestCase::new(Method::Put, uri)`
     pub fn put(uri: &str) -> Self {
         Self::new(Method::Put, uri)
     }
 
+    /// Equivalent to `TestCase::new(Method::Delete, uri)`
     pub fn delete(uri: &str) -> Self {
         Self::new(Method::Delete, uri)
     }
 
+    /// Equivalent to `TestCase::new(Method::Patch, uri)`
     pub fn patch(uri: &str) -> Self {
         Self::new(Method::Patch, uri)
     }
 
+    /// Set the HTTP header of this test case
     pub fn with_header<H: Header>(mut self, header: H) -> Self {
         self.request.headers.set(header);
         self
     }
 
+    /// Set the request body of this test case
     pub fn with_body<B: Into<Body>>(mut self, body: B) -> Self {
         self.body = Some(body.into());
         self
