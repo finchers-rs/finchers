3fdb50941f94a9201b1442a734d26f31245cfb25
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-08-22 18:45:11 +0900

    chore: update example

--- examples/todo.rs
+++ examples/todo.rs
@@ -10,110 +10,101 @@ use finchers::endpoint::{json_body, u64_};
 use finchers::endpoint::method::{delete, get, post, put};
 use finchers::response::Created;
 
-use std::collections::HashMap;
-use std::sync::RwLock;
+mod todo {
+    use std::collections::HashMap;
+    use std::sync::RwLock;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Todo {
-    id: u64,
-    title: String,
-    completed: bool,
-    order: usize,
-}
-
-#[derive(Deserialize)]
-pub struct NewTodo {
-    title: String,
-    completed: Option<bool>,
-    order: Option<usize>,
-}
-
-#[derive(Default)]
-pub struct Todos {
-    db: HashMap<u64, Todo>,
-    counter: u64,
-}
-
-impl Todos {
-    pub fn get(&self, id: u64) -> Option<Todo> {
-        self.db.get(&id).cloned()
+    #[derive(Clone, Debug, Serialize, Deserialize)]
+    pub struct Todo {
+        id: u64,
+        title: String,
+        completed: bool,
+        order: usize,
     }
 
-    pub fn get_mut(&mut self, id: u64) -> Option<&mut Todo> {
-        self.db.get_mut(&id)
+    #[derive(Deserialize)]
+    pub struct NewTodo {
+        title: String,
+        completed: Option<bool>,
+        order: Option<usize>,
     }
 
-    pub fn list(&self) -> Vec<Todo> {
-        self.db.iter().map(|i| i.1.clone()).collect()
+    #[derive(Default)]
+    struct Todos {
+        db: HashMap<u64, Todo>,
+        counter: u64,
     }
 
-    pub fn save(&mut self, new_todo: NewTodo) -> Todo {
-        self.counter += 1;
+    lazy_static! {
+        static ref TODOS: RwLock<Todos> = RwLock::new(Todos::default());
+    }
+
+    pub fn get(id: u64) -> Option<Todo> {
+        let todos = TODOS.read().unwrap();
+        todos.db.get(&id).cloned()
+    }
+
+    pub fn set(id: u64, new_todo: Todo) {
+        let mut todos = TODOS.write().unwrap();
+        if let Some(todo) = todos.db.get_mut(&id) {
+            *todo = new_todo;
+        }
+    }
+
+    pub fn list() -> Vec<Todo> {
+        let todos = TODOS.read().unwrap();
+        todos.db.iter().map(|i| i.1.clone()).collect()
+    }
+
+    pub fn save(new_todo: NewTodo) -> Todo {
+        let mut todos = TODOS.write().unwrap();
+        todos.counter += 1;
         let todo = Todo {
-            id: self.counter,
+            id: todos.counter,
             title: new_todo.title,
             completed: new_todo.completed.unwrap_or(false),
             order: new_todo.order.unwrap_or(0),
         };
 
-        self.db.insert(todo.id, todo.clone());
+        todos.db.insert(todo.id, todo.clone());
         todo
     }
 
-    pub fn delete(&mut self, id: u64) {
-        self.db.remove(&id);
+    pub fn delete(id: u64) {
+        let mut todos = TODOS.write().unwrap();
+        todos.db.remove(&id);
     }
 
-    pub fn clear(&mut self) {
-        self.db.clear();
+    pub fn clear() {
+        let mut todos = TODOS.write().unwrap();
+        todos.db.clear();
     }
 }
 
 fn main() {
-    lazy_static! {
-        pub static ref TODOS: RwLock<Todos> = RwLock::new(Todos::default());
-    }
-
     // GET /todos/:id
-    let get_todo = get("todos".with(u64_)).map(|id| {
-        let todos = TODOS.read().unwrap();
-        Json(todos.get(id))
-    });
+    let get_todo = get("todos".with(u64_)).map(|id| Json(todo::get(id)));
 
     // GET /todos
-    let get_todos = get("todos").map(|()| {
-        let todos = TODOS.read().unwrap();
-        Json(todos.list())
-    });
+    let get_todos = get("todos").map(|()| Json(todo::list()));
 
     // DELETE /todos/:id
-    let delete_todo = delete("todos".with(u64_)).map(|id| {
-        let mut todos = TODOS.write().unwrap();
-        todos.delete(id);
-    });
+    let delete_todo = delete("todos".with(u64_)).map(|id| { todo::delete(id); });
 
     // DELETE /todos
-    let delete_todos = delete("todos").map(|()| {
-        let mut todos = TODOS.write().unwrap();
-        todos.clear();
-    });
+    let delete_todos = delete("todos").map(|()| { todo::clear(); });
 
     // PUT /todos/:id
     let patch_todo = put("todos".with(u64_))
-        .join(json_body::<Todo>())
-        .map(|(id, Json(new_todo))| {
-            let mut todos = TODOS.write().unwrap();
-            if let Some(todo) = todos.get_mut(id) {
-                *todo = new_todo;
-            }
-        });
+        .join(json_body::<todo::Todo>())
+        .map(|(id, Json(new_todo))| { todo::set(id, new_todo); });
 
     // POST /todos
     let post_todo = post("todos")
-        .with(json_body::<NewTodo>())
+        .with(json_body::<todo::NewTodo>())
         .map(|Json(new_todo)| {
-            let mut todos = TODOS.write().unwrap();
-            Created(Json(todos.save(new_todo)))
+            let new_todo = todo::save(new_todo);
+            Created(Json(new_todo))
         });
 
     let endpoint = get_todo
