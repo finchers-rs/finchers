80eea269d9d8c0bf48d99c57f9b53c7d20e0a21e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-12-18 22:09:48 +0900

    update example

--- examples/hello.rs
+++ examples/hello.rs
@@ -2,15 +2,18 @@
 extern crate error_chain;
 extern crate finchers;
 
+use std::string::FromUtf8Error;
 use finchers::{Endpoint, EndpointError};
-use finchers::endpoint::method::get;
-use finchers::endpoint::param;
-use finchers::response::{Responder, Response, ResponseBuilder, StatusCode, ResponderContext};
+use finchers::endpoint::method::{get, post};
+use finchers::endpoint::{body, param};
+use finchers::task::BodyError;
+use finchers::response::{Responder, ResponderContext, Response, ResponseBuilder, StatusCode};
 use finchers::ServerBuilder;
 
 error_chain! {
     foreign_links {
         Endpoint(EndpointError);
+        Body(BodyError<FromUtf8Error>);
     }
 }
 
@@ -23,10 +26,16 @@ impl Responder for Error {
 }
 
 fn main() {
-    // GET /foo/:id/bar
-    let endpoint = get(("foo", param(), "bar"))
+    // GET /foo/:id
+    let endpoint1 = get(("foo", param()))
         .map(|(_, name, _)| name)
-        .and_then(|name: String| -> Result<_> { Ok(format!("Hello, {}", name)) });
+        .and_then(|(_, name): (_, String)| Ok(format!("Hello, {}", name)));
+
+    // POST /foo/:id [String] (Content-type: text/plain; charset=utf-8)
+    let endpoint2 = post(("foo", param(), body()))
+        .and_then(|(name, body): (_, String, String)| Ok(format!("Hello, {} ({})", name, body)));
+
+    let endpoint = endpoint1.or(endpoint2).with_type::<_, Error>();
 
     ServerBuilder::default()
         .bind("0.0.0.0:8080")
