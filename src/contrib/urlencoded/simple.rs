740fb8e2a0f7313bbf2216b31f7242b71934aeee
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-01-20 21:09:24 +0900

    feat(contrib): add json_body/form_body

--- src/contrib/urlencoded/simple.rs
+++ src/contrib/urlencoded/simple.rs
@@ -7,10 +7,10 @@ use std::collections::HashMap;
 use std::fmt;
 use std::error::Error;
 use std::marker::PhantomData;
-use futures::IntoFuture;
-use futures::future::FutureResult;
+use futures::future::{self, Future, FutureResult, IntoFuture};
 
-use endpoint::{Endpoint, EndpointContext, EndpointResult};
+use endpoint::{self, Endpoint, EndpointContext, EndpointResult};
+use endpoint::body::BodyError;
 use errors::StdErrorResponseBuilder;
 use http::{self, mime, FromBody, IntoResponse, Request, Response};
 
@@ -390,5 +390,51 @@ impl<F: FromUrlEncoded> FromBody for Form<F> {
     fn from_body(body: Vec<u8>) -> Result<Self, Self::Error> {
         let iter = self::url::form_urlencoded::parse(&body);
         F::from_urlencoded(iter).map(Form)
+    }
+}
+
+#[allow(missing_docs)]
+pub fn form_body<T: FromUrlEncoded>() -> FormBody<T> {
+    FormBody {
+        inner: endpoint::body::body(),
+    }
+}
+
+#[allow(missing_docs)]
+pub struct FormBody<T> {
+    inner: endpoint::body::Body<Form<T>>,
+}
+
+impl<T> fmt::Debug for FormBody<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_tuple("FormBody").field(&self.inner).finish()
+    }
+}
+
+impl<T: FromUrlEncoded> Endpoint for FormBody<T> {
+    type Item = T;
+    type Error = BodyError<Form<T>>;
+    type Result = FormBodyResult<T>;
+
+    fn apply(&self, ctx: &mut EndpointContext) -> Option<Self::Result> {
+        Some(FormBodyResult {
+            inner: try_opt!(self.inner.apply(ctx)),
+        })
+    }
+}
+
+#[doc(hidden)]
+#[allow(missing_debug_implementations)]
+pub struct FormBodyResult<T> {
+    inner: endpoint::body::BodyResult<Form<T>>,
+}
+
+impl<T: FromUrlEncoded> EndpointResult for FormBodyResult<T> {
+    type Item = T;
+    type Error = BodyError<Form<T>>;
+    type Future = future::Map<endpoint::body::BodyFuture<Form<T>>, fn(Form<T>) -> T>;
+
+    fn into_future(self, request: &mut Request) -> Self::Future {
+        self.inner.into_future(request).map(|Form(body)| body)
     }
 }
