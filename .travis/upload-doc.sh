418d4a2cf59f3f5661d843e2171b212e17f24408
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-02-02 01:38:00 +0900

    fix(ci): update .travis.yml

---
+++ .travis/upload-doc.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -euo pipefail
+
+get-branch() {
+    if [[ "${TRAVIS_PULL_REQUEST:-}" == false ]]; then
+        echo "${TRAVIS_BRANCH}"
+    else
+        echo "${TRAVIS_PULL_REQUEST_BRANCH}"
+    fi
+}
+
+if [[ -z "${TRAVIS_BRANCH:-}" ]]; then
+    echo "This script may only be running from Travis CI."
+    exit 1
+fi
+
+BRANCH="$(get-branch)"
+if [[ "${BRANCH}" != "master" ]]; then
+    echo "The deployment should be from 'master', not '${BRANCH}'."
+    exit 1
+fi
+
+REV="$(git rev-parse --short HEAD)"
+UPSTREAM_URL="https://${GH_TOKEN}@github.com/finchers-rs/finchers.git"
+USERNAME="Yusuke Sasaki"
+EMAIL="yusuke.sasaki.nuem@gmail.com"
+
+echo "Commiting docs directory to gh-pages branch"
+cd target/doc
+git init
+git remote add upstream "${UPSTREAM_URL}"
+git config user.name "${USERNAME}"
+git config user.email "${EMAIL}"
+git add -A .
+git commit -qm "Build documentation at ${TRAVIS_REPO_SLUG}@${REV}"
+
+echo "Pushing gh-pages to GitHub"
+git push -q upstream HEAD:refs/heads/gh-pages --force
