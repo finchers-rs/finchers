52ebcd3b50821a894f3e64911c02949a8d40d7b3
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-02-05 06:02:01 +0900

    chore(*): change the destination URL of deployed docs

--- .travis/build-doc.sh
+++ .travis/build-doc.sh
@@ -2,25 +2,35 @@
 
 set -eux
 
-DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-cd "${DIR}/.."
+DIR=$(cd "$(dirname ${BASH_SOURCE[0]})"/../ && pwd)
+REV="$(git rev-parse --short HEAD)"
+USERNAME="Yusuke Sasaki"
+EMAIL="yusuke.sasaki.nuem@gmail.com"
 
 # remove all contents at destination directory
+cd "${DIR}"
 rm -rf target/doc-upload
 mkdir -p target/doc-upload
 
 # homepage
-pushd site/ > /dev/null
+cd "${DIR}/site"
 bundle install
-bundle exec jekyll build -d ../target/doc-upload
-popd > /dev/null
+bundle exec jekyll build -d "${DIR}/target/doc-upload"
 
 # API doc
-rm -rf target/doc
+cd "${DIR}"
+rm -rf ./target/doc
 cargo doc --all --no-deps --features "$STABLE_FEATURES"
-cp -a target/doc target/doc-upload/api
+cp -a ./target/doc ./target/doc-upload/api
 
 # users guide
-pushd guide/ > /dev/null
+cd "${DIR}/guide"
 mdbook build
-popd > /dev/null
+
+# commit
+cd "${DIR}/target/doc-upload"
+git init
+git config user.name "${USERNAME}"
+git config user.email "${EMAIL}"
+git add -A .
+git commit -qm "Build documentation at ${TRAVIS_REPO_SLUG}@${REV}"
