0e3dd9e90ede49c7f110190628af2e3247420605
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-04-17 23:19:11 +0900

    fix(core): remove implementors of HttpError from stds

--- finchers-core/src/error.rs
+++ finchers-core/src/error.rs
@@ -14,42 +14,6 @@ impl HttpError for ! {
     }
 }
 
-macro_rules! impl_http_error {
-    (@bad_request) => { StatusCode::BAD_REQUEST };
-    (@server_error) => { StatusCode::INTERNAL_SERVER_ERROR };
-
-    ($( @$i:ident $t:ty; )*) => {$(
-        impl HttpError for $t {
-            #[inline]
-            fn status_code(&self) -> StatusCode {
-                impl_http_error!(@$i)
-            }
-        }
-    )*};
-}
-
-impl_http_error! {
-    @bad_request ::std::char::DecodeUtf16Error;
-    @bad_request ::std::char::ParseCharError;
-    @bad_request ::std::net::AddrParseError;
-    @bad_request ::std::num::ParseFloatError;
-    @bad_request ::std::num::ParseIntError;
-    @bad_request ::std::str::Utf8Error;
-    @bad_request ::std::str::ParseBoolError;
-    @bad_request ::std::string::ParseError;
-    @bad_request ::std::string::FromUtf8Error;
-    @bad_request ::std::string::FromUtf16Error;
-
-    @server_error ::std::cell::BorrowError;
-    @server_error ::std::cell::BorrowMutError;
-    @server_error ::std::env::VarError;
-    @server_error ::std::fmt::Error;
-    @server_error ::std::io::Error;
-    @server_error ::std::sync::mpsc::RecvError;
-    @server_error ::std::sync::mpsc::TryRecvError;
-    @server_error ::std::sync::mpsc::RecvTimeoutError;
-}
-
 #[derive(Debug)]
 pub struct BadRequest<E> {
     err: E,
