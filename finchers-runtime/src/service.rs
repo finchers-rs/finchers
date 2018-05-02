ce4fe2355557ca5bf5cb4b35a5503375c72453b6
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-05-02 17:18:40 +0900

    refactor(runtime): add NewHttpService and adapt with it

--- finchers-runtime/src/service.rs
+++ finchers-runtime/src/service.rs
@@ -1,21 +1,43 @@
-//! Components of lower-level HTTP services
-
-use bytes::Bytes;
-use futures::Async::*;
-use futures::{self, Future, Stream};
-use http::StatusCode;
-use http::header::{self, HeaderValue};
+use futures::Future;
 use http::{Request, Response};
 use std::sync::Arc;
-use std::{fmt, io};
 
-use finchers_core::endpoint::ApplyRequest;
-use finchers_core::error::Error;
-use finchers_core::input::RequestBody;
-use finchers_core::output::{Responder, ResponseBody};
-use finchers_core::{Endpoint, HttpError, Input, Poll, Task};
+pub trait NewHttpService {
+    type RequestBody;
+    type ResponseBody;
+    type Error;
+    type Service: HttpService<RequestBody = Self::RequestBody, ResponseBody = Self::ResponseBody, Error = Self::Error>;
+    type InitError;
+    type Future: Future<Item = Self::Service, Error = Self::InitError>;
 
-// ==== HttpService ====
+    fn new_service(&self) -> Self::Future;
+}
+
+impl<S: NewHttpService> NewHttpService for Box<S> {
+    type RequestBody = S::RequestBody;
+    type ResponseBody = S::ResponseBody;
+    type Error = S::Error;
+    type Service = S::Service;
+    type Future = S::Future;
+    type InitError = S::InitError;
+
+    fn new_service(&self) -> Self::Future {
+        (**self).new_service()
+    }
+}
+
+impl<S: NewHttpService> NewHttpService for Arc<S> {
+    type RequestBody = S::RequestBody;
+    type ResponseBody = S::ResponseBody;
+    type Error = S::Error;
+    type Service = S::Service;
+    type Future = S::Future;
+    type InitError = S::InitError;
+
+    fn new_service(&self) -> Self::Future {
+        (**self).new_service()
+    }
+}
 
 #[allow(missing_docs)]
 pub trait HttpService {
@@ -24,7 +46,7 @@ pub trait HttpService {
     type Error;
     type Future: Future<Item = Response<Self::ResponseBody>, Error = Self::Error>;
 
-    fn call(&self, request: Request<Self::RequestBody>) -> Self::Future;
+    fn call(&mut self, request: Request<Self::RequestBody>) -> Self::Future;
 }
 
 impl<S: HttpService> HttpService for Box<S> {
@@ -33,150 +55,7 @@ impl<S: HttpService> HttpService for Box<S> {
     type Error = S::Error;
     type Future = S::Future;
 
-    fn call(&self, request: Request<Self::RequestBody>) -> Self::Future {
+    fn call(&mut self, request: Request<Self::RequestBody>) -> Self::Future {
         (**self).call(request)
     }
-}
-
-impl<S: HttpService> HttpService for Arc<S> {
-    type RequestBody = S::RequestBody;
-    type ResponseBody = S::ResponseBody;
-    type Error = S::Error;
-    type Future = S::Future;
-
-    fn call(&self, request: Request<Self::RequestBody>) -> Self::Future {
-        (**self).call(request)
-    }
-}
-
-/// An HTTP service which wraps an `Endpoint`.
-pub struct EndpointService<E> {
-    endpoint: E,
-    error_handler: ErrorHandler,
-}
-
-impl<E> EndpointService<E> {
-    pub fn new(endpoint: E) -> EndpointService<E> {
-        Self {
-            endpoint,
-            error_handler: default_error_handler,
-        }
-    }
-
-    pub fn set_error_handler(&mut self, handler: ErrorHandler) {
-        self.error_handler = handler;
-    }
-}
-
-impl<E> HttpService for EndpointService<E>
-where
-    E: Endpoint,
-    E::Output: Responder,
-{
-    type RequestBody = RequestBody;
-    type ResponseBody = BodyStream;
-    type Error = io::Error;
-    type Future = EndpointServiceFuture<E::Task>;
-
-    fn call(&self, request: Request<Self::RequestBody>) -> Self::Future {
-        let (parts, body) = request.into_parts();
-        let input = Input::new(Request::from_parts(parts, ()));
-        let apply = self.endpoint.apply_request(&input, body);
-
-        EndpointServiceFuture {
-            apply,
-            input,
-            error_handler: self.error_handler,
-        }
-    }
-}
-
-#[allow(missing_debug_implementations)]
-pub struct EndpointServiceFuture<T> {
-    apply: ApplyRequest<T>,
-    input: Input,
-    error_handler: ErrorHandler,
-}
-
-impl<T> EndpointServiceFuture<T> {
-    fn handle_error(&self, err: &HttpError) -> Response<ResponseBody> {
-        (self.error_handler)(err, &self.input)
-    }
-}
-
-impl<T> Future for EndpointServiceFuture<T>
-where
-    T: Task,
-    T::Output: Responder,
-{
-    type Item = Response<BodyStream>;
-    type Error = io::Error;
-
-    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
-        let mut response = match self.apply.poll_ready(&self.input) {
-            Poll::Pending => return Ok(NotReady),
-            Poll::Ready(Some(Ok(output))) => output
-                .respond(&self.input)
-                .unwrap_or_else(|err| self.handle_error(Into::<Error>::into(err).as_http_error())),
-            Poll::Ready(Some(Err(err))) => self.handle_error(err.as_http_error()),
-            Poll::Ready(None) => self.handle_error(&NoRoute),
-        };
-
-        if !response.headers().contains_key(header::SERVER) {
-            response.headers_mut().insert(
-                header::SERVER,
-                HeaderValue::from_static(concat!("finchers-runtime/", env!("CARGO_PKG_VERSION"))),
-            );
-        }
-
-        Ok(Ready(response.map(BodyStream)))
-    }
-}
-
-#[derive(Debug)]
-struct NoRoute;
-
-impl fmt::Display for NoRoute {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("no route")
-    }
-}
-
-impl HttpError for NoRoute {
-    fn status_code(&self) -> StatusCode {
-        StatusCode::NOT_FOUND
-    }
-}
-
-#[derive(Debug)]
-pub struct BodyStream(ResponseBody);
-
-impl Stream for BodyStream {
-    type Item = Bytes;
-    type Error = io::Error;
-
-    fn poll(&mut self) -> futures::Poll<Option<Self::Item>, Self::Error> {
-        self.0.poll_data().into()
-    }
-}
-
-///
-pub type ErrorHandler = fn(&HttpError, &Input) -> Response<ResponseBody>;
-
-fn default_error_handler(err: &HttpError, _: &Input) -> Response<ResponseBody> {
-    let body = err.to_string();
-    let body_len = body.len().to_string();
-
-    let mut response = Response::new(ResponseBody::once(body));
-    *response.status_mut() = err.status_code();
-    response.headers_mut().insert(
-        header::CONTENT_TYPE,
-        HeaderValue::from_static("text/plain; charset=utf-8"),
-    );
-    response.headers_mut().insert(header::CONTENT_LENGTH, unsafe {
-        HeaderValue::from_shared_unchecked(body_len.into())
-    });
-    err.append_headers(response.headers_mut());
-
-    response
 }
