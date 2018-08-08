9bb1fd2c0bf6079f331d86bec8d749ba52ff3cec
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-08-09 08:22:11 +0900

    add responders

--- finchers-core/src/output.rs
+++ finchers-core/src/output.rs
@@ -3,20 +3,23 @@
 pub mod payloads;
 pub mod responders;
 
-use http::Response;
+use http::{Response, StatusCode};
 use hyper::body::Payload;
 
+use self::payloads::Empty;
 use crate::either::Either;
-use crate::error::{Error, Never};
+use crate::error::{Error, Never, NoRoute};
 use crate::input::Input;
 
-/// Trait representing the conversion to an HTTP response.
-#[allow(missing_docs)]
+/// Trait representing types to be converted into an HTTP response.
 pub trait Responder {
+    /// The type of message body in the HTTP response to the client.
     type Body: Payload;
+
+    /// The error type which will be returned from `respond()`.
     type Error: Into<Error>;
 
-    /// Consume `self` and construct a new HTTP response.
+    /// Performs conversion this value into an HTTP response.
     fn respond(self, input: &Input) -> Result<Response<Self::Body>, Self::Error>;
 }
 
@@ -27,6 +30,32 @@ impl<T: Payload> Responder for Response<T> {
     #[inline(always)]
     fn respond(self, _: &Input) -> Result<Response<Self::Body>, Self::Error> {
         Ok(self)
+    }
+}
+
+impl Responder for () {
+    type Body = Empty;
+    type Error = Never;
+
+    fn respond(self, _: &Input) -> Result<Response<Self::Body>, Self::Error> {
+        Ok(Response::builder()
+            .status(StatusCode::NO_CONTENT)
+            .body(Empty)
+            .unwrap())
+    }
+}
+
+impl<T> Responder for Option<T>
+where
+    T: Responder,
+{
+    type Body = T::Body;
+    type Error = Error;
+
+    fn respond(self, input: &Input) -> Result<Response<Self::Body>, Self::Error> {
+        self.ok_or_else(|| NoRoute)?
+            .respond(input)
+            .map_err(Into::into)
     }
 }
 
