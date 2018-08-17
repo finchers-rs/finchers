9125ce1490a0a5135e74ee97fa290470839f2c54
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-08-17 21:38:12 +0900

    refactor App and AppFuture

--- src/rt/mod.rs
+++ src/rt/mod.rs
@@ -2,59 +2,90 @@
 
 pub mod local;
 
-mod app;
-mod server;
+use failure;
+use futures::Future;
+use hyper::server::Server;
+use log::{error, info, log};
+use std::net::{IpAddr, SocketAddr};
+use structopt::StructOpt;
+use tokio;
 
-pub use self::server::{launch, LaunchResult};
+use crate::app::App;
+use crate::endpoint::Endpoint;
+use crate::output::Responder;
 
-/// A type alias represents endpoints which can be used as an HTTP application.
-pub trait AppEndpoint: sealed::Sealed {}
+#[derive(Debug, StructOpt)]
+#[structopt(name = "finchers")]
+struct Cli {
+    /// The host of listener address.
+    #[structopt(short = "h", long = "host", default_value = "127.0.0.1")]
+    host: IpAddr,
 
-mod sealed {
-    use super::AppEndpoint;
+    /// The port of listener address.
+    #[structopt(short = "p", long = "port", default_value = "5000")]
+    port: u16,
+}
 
-    use futures_core::future::TryFuture;
-    use std::mem::PinMut;
+/// A set of configurations used by the runtime.
+#[derive(Debug)]
+pub struct Config {
+    addr: SocketAddr,
+    cli: Option<Cli>,
+}
 
-    use crate::endpoint::Endpoint;
-    use crate::error::Error;
-    use crate::input::{Cursor, Input};
-    use crate::output::Responder;
-
-    pub trait Sealed: Send + Sync + 'static {
-        type Output: Responder;
-        type Future: TryFuture<Ok = Self::Output, Error = Error> + Send + 'static;
-
-        fn apply<'c>(
-            &self,
-            input: PinMut<'_, Input>,
-            cursor: Cursor<'c>,
-        ) -> Option<(Self::Future, Cursor<'c>)>;
-    }
-
-    impl<E> Sealed for E
-    where
-        E: Endpoint + Send + Sync + 'static,
-        E::Output: Responder,
-        E::Future: Send + 'static,
-    {
-        type Output = E::Output;
-        type Future = E::Future;
-
-        #[inline(always)]
-        fn apply<'c>(
-            &self,
-            input: PinMut<'_, Input>,
-            cursor: Cursor<'c>,
-        ) -> Option<(Self::Future, Cursor<'c>)> {
-            Endpoint::apply(self, input, cursor)
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            addr: ([127, 0, 0, 1], 5000).into(),
+            cli: None,
         }
     }
+}
 
-    impl<E> AppEndpoint for E
-    where
-        E: Endpoint + Send + Sync + 'static,
-        E::Output: Responder,
-        E::Future: Send + 'static,
-    {}
+impl Config {
+    /// Create an instance of "Config" from the environment.
+    pub fn from_env() -> Config {
+        let mut config = Config::default();
+        config.overwite_cli(Cli::from_args());
+        config
+    }
+
+    fn overwite_cli(&mut self, cli: Cli) {
+        self.addr = (cli.host, cli.port).into();
+        self.cli = Some(cli);
+    }
+
+    #[allow(missing_docs)]
+    pub fn addr(&self) -> SocketAddr {
+        self.addr
+    }
+}
+
+#[allow(missing_docs)]
+pub type LaunchResult<T> = Result<T, failure::Error>;
+
+/// Start the server with given endpoint and default configuration.
+pub fn launch<E>(endpoint: E) -> LaunchResult<()>
+where
+    E: Endpoint + Send + Sync + 'static,
+    E::Output: Responder,
+    E::Future: Send + 'static,
+{
+    let config = Config::from_env();
+
+    let endpoint: &'static _ = unsafe { &*(&endpoint as *const _) };
+    let new_service = App::new(endpoint);
+
+    let server = Server::try_bind(&config.addr())?
+        .serve(new_service)
+        .map_err({
+            move |err| {
+                error!("server error: {}", err);
+            }
+        });
+
+    info!("Listening on http://{}", config.addr());
+    tokio::run(server);
+
+    Ok(())
 }
