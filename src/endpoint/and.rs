8f55f4e4bb4f2fce0e1603e4c2b9a38506f74c08
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-09-06 19:02:48 +0900

    fix `And`

--- src/endpoint/and.rs
+++ src/endpoint/and.rs
@@ -3,9 +3,8 @@ use std::pin::PinMut;
 use std::task;
 use std::task::Poll;
 
-use futures_core::future::Future;
-use futures_util::future::{maybe_done, MaybeDone};
-use futures_util::try_future::{IntoFuture, TryFutureExt};
+use futures_core::future::{Future, TryFuture};
+use futures_util::try_future::{TryFutureExt, TryJoin};
 use pin_utils::unsafe_pinned;
 
 use crate::common::{Combine, Tuple};
@@ -26,68 +25,60 @@ where
     E1::Output: Combine<E2::Output>,
 {
     type Output = <E1::Output as Combine<E2::Output>>::Out;
-    type Future = AndFuture<IntoFuture<E1::Future>, IntoFuture<E2::Future>>;
+    type Future = AndFuture<E1::Future, E2::Future>;
 
     fn apply(&'a self, ecx: &mut Context<'_>) -> EndpointResult<Self::Future> {
         let f1 = self.e1.apply(ecx)?;
         let f2 = self.e2.apply(ecx)?;
         Ok(AndFuture {
-            f1: maybe_done(f1.into_future()),
-            f2: maybe_done(f2.into_future()),
+            inner: f1.try_join(f2),
         })
     }
 }
 
-pub struct AndFuture<F1: Future, F2: Future> {
-    f1: MaybeDone<F1>,
-    f2: MaybeDone<F2>,
+pub struct AndFuture<F1, F2>
+where
+    F1: TryFuture<Error = Error>,
+    F2: TryFuture<Error = Error>,
+{
+    inner: TryJoin<F1, F2>,
 }
 
 impl<F1, F2> fmt::Debug for AndFuture<F1, F2>
 where
-    F1: Future + fmt::Debug,
-    F2: Future + fmt::Debug,
-    F1::Output: fmt::Debug,
-    F2::Output: fmt::Debug,
+    F1: TryFuture<Error = Error> + fmt::Debug,
+    F2: TryFuture<Error = Error> + fmt::Debug,
+    F1::Ok: fmt::Debug,
+    F2::Ok: fmt::Debug,
 {
     fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
         formatter
             .debug_struct("AndFuture")
-            .field("f1", &self.f1)
-            .field("f2", &self.f2)
+            .field("inner", &self.inner)
             .finish()
     }
 }
 
-impl<F1: Future, F2: Future> AndFuture<F1, F2> {
-    unsafe_pinned!(f1: MaybeDone<F1>);
-    unsafe_pinned!(f2: MaybeDone<F2>);
+impl<F1, F2> AndFuture<F1, F2>
+where
+    F1: TryFuture<Error = Error>,
+    F2: TryFuture<Error = Error>,
+{
+    unsafe_pinned!(inner: TryJoin<F1, F2>);
 }
 
-impl<F1, F2, T1, T2> Future for AndFuture<F1, F2>
+impl<F1, F2> Future for AndFuture<F1, F2>
 where
-    F1: Future<Output = Result<T1, Error>>,
-    F2: Future<Output = Result<T2, Error>>,
-    T1: Tuple + Combine<T2>,
-    T2: Tuple,
+    F1: TryFuture<Error = Error>,
+    F2: TryFuture<Error = Error>,
+    F1::Ok: Tuple + Combine<F2::Ok>,
+    F2::Ok: Tuple,
 {
-    type Output = Result<<T1 as Combine<T2>>::Out, Error>;
+    type Output = Result<<F1::Ok as Combine<F2::Ok>>::Out, Error>;
 
     fn poll(mut self: PinMut<'_, Self>, cx: &mut task::Context<'_>) -> Poll<Self::Output> {
-        // FIXME: early return if MaybeDone::poll(cx) returns an Err.
-        let all_done = !self.f1().poll(cx).is_pending() && !self.f2().poll(cx).is_pending();
-        if all_done {
-            let v1 = match self.f1().take_output().unwrap() {
-                Ok(v) => v,
-                Err(e) => return Poll::Ready(Err(e)),
-            };
-            let v2 = match self.f2().take_output().unwrap() {
-                Ok(v) => v,
-                Err(e) => return Poll::Ready(Err(e)),
-            };
-            Poll::Ready(Ok(Combine::combine(v1, v2)))
-        } else {
-            Poll::Pending
-        }
+        self.inner()
+            .poll(cx)
+            .map_ok(|(v1, v2)| Combine::combine(v1, v2))
     }
 }
