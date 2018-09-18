21af03e69357c7992a0cc14a7c5ed310da4868d4
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-09-19 01:03:26 +0900

    add payload::Optional<T>

--- src/output/payload.rs
+++ src/output/payload.rs
@@ -1,6 +1,8 @@
 //! Implementors of `Payload`.
 
+use either::Either;
 use futures::{self, Async};
+use http::header::HeaderMap;
 use std::io;
 
 pub use hyper::body::{Body, Payload};
@@ -47,5 +49,72 @@ impl<T: AsRef<[u8]> + Send + 'static> Payload for Once<T> {
 
     fn content_length(&self) -> Option<u64> {
         self.0.as_ref().map(|body| body.as_ref().len() as u64)
+    }
+}
+
+/// An instance of `Payload` which acts either a certain data or an empty bytes.
+#[derive(Debug)]
+pub struct Optional<T>(Either<T, bool>);
+
+impl<T> From<T> for Optional<T> {
+    fn from(data: T) -> Optional<T> {
+        Optional::new(data)
+    }
+}
+
+impl<T> From<Option<T>> for Optional<T> {
+    fn from(data: Option<T>) -> Optional<T> {
+        match data {
+            Some(data) => Optional::new(data),
+            None => Optional::empty(),
+        }
+    }
+}
+
+impl<T> Optional<T> {
+    /// Create an empty `Optional<T>`.
+    pub fn empty() -> Optional<T> {
+        Optional(Either::Right(false))
+    }
+
+    /// Create an `Optional<T>` from a value of `T`.
+    pub fn new(data: T) -> Optional<T> {
+        Optional(Either::Left(data))
+    }
+}
+
+impl<T: Payload> Payload for Optional<T> {
+    type Data = Either<T::Data, io::Cursor<[u8; 0]>>;
+    type Error = T::Error;
+
+    fn poll_data(&mut self) -> futures::Poll<Option<Self::Data>, Self::Error> {
+        match self.0 {
+            Either::Left(ref mut payload) => payload
+                .poll_data()
+                .map(|data_async| data_async.map(|data_opt| data_opt.map(Either::Left))),
+            Either::Right(false) => Ok(Some(Either::Right(io::Cursor::new([]))).into()),
+            Either::Right(true) => Ok(None.into()),
+        }
+    }
+
+    fn poll_trailers(&mut self) -> futures::Poll<Option<HeaderMap>, Self::Error> {
+        match self.0 {
+            Either::Left(ref mut payload) => payload.poll_trailers(),
+            Either::Right(..) => Ok(None.into()),
+        }
+    }
+
+    fn is_end_stream(&self) -> bool {
+        match self.0 {
+            Either::Left(ref payload) => payload.is_end_stream(),
+            Either::Right(is_end_stream) => is_end_stream,
+        }
+    }
+
+    fn content_length(&self) -> Option<u64> {
+        match self.0 {
+            Either::Left(ref payload) => payload.content_length(),
+            Either::Right(..) => Some(0),
+        }
     }
 }
